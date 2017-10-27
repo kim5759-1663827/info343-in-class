f9a9288c8c432d42d48e4be7ace85a9c0e8fea96
Hyewon Kim <kim5759@uw.edu>
2017-10-26 20:32:35 -0700

    during in class

--- labs/javascript/js/lab.js
+++ labs/javascript/js/lab.js
@@ -1,2 +1,37 @@
 // Put your JavaScript lab code here!
 
+//Strings
+var str = "I like JavaScript more than I like to party. ";
+var res1 = str.slice(28,45);
+
+var str = "I love Washington State University."
+var res2 = str.replace("Washington State University", "The University of Washington");
+
+var res = res1.concat(res2);
+
+//Functions
+function sumUpTo(n) {
+    var total = 0;
+    for (var i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+function vowelCount(string) {
+    return string.match(/[aeiou]/gi).length;
+}
+
+//Arrays
+function findMin(Arrays) {
+    return Math.min.apply(null, Arrays);
+}
+
+//Objects
+var title = ["Harry Potter and the Goblet of Fire", "The Great Gastby", "The Hunger Games"];
+var author = ["J.K.Rowling", "F. Scott Fitzgerald", "Suzanne Collins"];
+var read = ["have read", "have not read", "have not read"];
+
+title.push("1984");
+author.push("George Orwell");
+read.push("have read");
